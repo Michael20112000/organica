3743a3366700fcce8424b66e5ce75a762313b7d7
michael_front <mikhail_front@corpcastle.com>
2026-03-25 19:17:23 +0200

    added no-restricted-imports rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,32 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          name: 'next/link',
+          message: 'Please import from `@/i18n/navigation` instead.',
+        },
+        {
+          name: 'next/navigation',
+          importNames: [
+            'redirect',
+            'permanentRedirect',
+            'useRouter',
+            'usePathname',
+          ],
+          message: 'Please import from `@/i18n/navigation` instead.',
+        },
+        {
+          name: '@/types/auto-generated-strapi-types',
+          message: 'Please define types in @/types/entity-types file and import from `@/types`.',
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
