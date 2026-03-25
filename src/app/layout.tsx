b9063002327c0cbb4c52d415860a51a47b1637d8
michael_front <mikhail_front@corpcastle.com>
2026-03-25 13:56:46 +0200

    added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
 import type { FC, PropsWithChildren } from 'react'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter_Tight, Playfair_Display } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+const interTight = Inter_Tight({
+  variable: '--font-inter-tight',
 })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const playfairDisplay = Playfair_Display({
+  variable: '--font-playfair-display',
+  style: ['italic', 'normal'],
 })
 
 const RootLayout: FC<PropsWithChildren> = props => {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${interTight.variable} ${playfairDisplay.variable}`}>
         {props.children}
       </body>
     </html>
