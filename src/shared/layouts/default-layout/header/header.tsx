1399454faf9504376e4c5eb9b85dd0c17b95e1b5
michael_front <mikhail_front@corpcastle.com>
2026-03-25 19:21:10 +0200

    test build error x2

--- src/shared/layouts/default-layout/header/header.tsx
+++ src/shared/layouts/default-layout/header/header.tsx
@@ -1,32 +1,34 @@
-// import { Link } from '@/i18n/navigation'
+import { Link } from '@/i18n/navigation'
 // import { Logotype } from '@/shared/components'
-// import {
-//   getHeaderNavigationLinks,
-//   type NavigationLink,
-// } from './get-header-navigation-links'
+import {
+  getHeaderNavigationLinks,
+  type NavigationLink,
+} from './get-header-navigation-links'
 // import { Burger } from './burger'
 import { styles } from './styles'
 
 export const Header = async () => {
-  // const { firstNavigationPart, secondNavigationBarPart } =
-  //   await getHeaderNavigationLinks()
-  //
-  // const renderNavigationLinks = (links: NavigationLink[]) => {
-  //   return links.map(link => (
-  //     <Link key={link.href} className={styles.link} href={link.href}>
-  //       {link.text}
-  //     </Link>
-  //   ))
-  // }
+  const {
+    firstNavigationPart,
+    // secondNavigationBarPart
+  } = await getHeaderNavigationLinks()
+
+  const renderNavigationLinks = (links: NavigationLink[]) => {
+    return links.map(link => (
+      <Link key={link.href} className={styles.link} href={link.href}>
+        {link.text}
+      </Link>
+    ))
+  }
 
   return (
     <header className={styles.header}>
-      {/*<nav className={styles.firstNavBarPart}>*/}
-      {/*  <Link className={styles.link} href='/'>*/}
-      {/*    UA / EN*/}
-      {/*  </Link>*/}
-      {/*  {renderNavigationLinks(firstNavigationPart)}*/}
-      {/*</nav>*/}
+      <nav className={styles.firstNavBarPart}>
+        <Link className={styles.link} href='/'>
+          UA / EN
+        </Link>
+        {renderNavigationLinks(firstNavigationPart)}
+      </nav>
 
       {/*<Link href='/'>*/}
       {/*  <Logotype className={styles.logotype} />*/}
